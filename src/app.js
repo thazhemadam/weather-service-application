6be38db8e43bc84f37ce049a2c2916ca97b2935f
Anant Thazhemadam <anant.thazhemadam@gmail.com>
2020-05-04 12:11:23 +0530

    Readying for Production

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const path = require('path')
-if (process.env.NODE_ENV !== 'production') require("dotenv").config({ path: path.resolve(__dirname, `../config/${process.env.NODE_ENV}.env`)})const express = require('express')
+if (process.env.NODE_ENV !== 'production') require("dotenv").config({ path: path.resolve(__dirname, `../config/${process.env.NODE_ENV}.env`)})
+const express = require('express')
 const hbs = require('hbs')
 const targetLocation = require('./utils/geocode')
 const forecast = require('./utils/forecast')
